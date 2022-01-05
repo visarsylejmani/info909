c92e61c815e4a3e6db19f55d6d5d8cb83cef8bad
Visar <Visar@DESKTOP-8A09G93>
2022-01-05 09:33:54 +0100

    hoey

--- automatisation/script.py
+++ automatisation/script.py
@@ -1,13 +1,20 @@
 import os
 import time
+from decouple import config
+
+GITHUB_USER = config('GITHUB_USER')
+GITHUB_TOKEN = config('GITHUB_TOKEN')
 
 while (True):
-    os.system('echo $(date -u)  "Login to github"')
-    os.system('docker login -u visarsylejmani -p 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 docker.pkg.github.com')
+    os.system('echo $(date -u)  "Login to GITHUB_TOKENgithub"')
+    t = f'docker login -u {GITHUB_USER} -p {GITHUB_TOKEN} docker.pkg.github.com'
+    print(t)
+    os.system(t)
     os.system('echo $(date -u) "Refresh docker images"')
     os.system('docker-compose pull --include-deps')
     os.system('echo $(date -u) "Refresh docker images"')
-    os.system('docker-compose -f /home/visar/webhook/click-and-work/docker-compose.yml -p click-and-work up -d --build --remove-orphans --force-recreate')
+    os.system(
+        'docker-compose -p genie-log up -d --build --remove-orphans --force-recreate')
     os.system('echo $(date -u) "Kill current containers"')
     os.system('docker-compose down')
     os.system('echo $(date -u) "Refresh docker containers"')
