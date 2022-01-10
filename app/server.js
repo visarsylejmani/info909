14bfd51a5fb42693716d1fa87bb96142119108d2
Visar <Visar@DESKTOP-8A09G93>
2022-01-10 12:20:43 +0100

    mise à jour

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 
 // Constants
 const PORT = 8080;
@@ -6,9 +7,41 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(express.json());
 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.post('/add', function (req, res) {
+  var num1 = parseFloat(req.body.num1)
+  var num2 = parseFloat(req.body.num2)
+  var total = (num1 + num2).toString()
+  return res.status(200).send(total)
+})
+
+app.post('/sub', function (req, res) {
+  var num1 = parseFloat(req.body.num1)
+  var num2 = parseFloat(req.body.num2)
+  var total = (num1 - num2).toString()
+  return res.status(200).send(total)
+})
+
+app.post('/mul', function (req, res) {
+  var num1 = parseFloat(req.body.num1)
+  var num2 = parseFloat(req.body.num2)
+  var total = (num1 * num2).toString()
+  return res.status(200).send(total)
+})
+
+app.post('/div', function (req, res) {
+  var num1 = parseFloat(req.body.num1)
+  var num2 = parseFloat(req.body.num2)
+  if (num2 === 0)
+    return res.status(200).send("Cannot devide by 0")
+  var total = (num1 / num2).toString()
+  return res.status(200).send(total)
+})
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
